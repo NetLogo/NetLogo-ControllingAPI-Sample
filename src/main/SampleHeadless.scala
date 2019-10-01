2f4b47e33addf97efd977a3996d8ce69a023ddf2
LaCuneta <jeremy.baker@northwestern.edu>
2019-10-01 12:19:36 -0500

    Add a two-workspace example

--- src/main/SampleHeadless.scala
+++ src/main/SampleHeadless.scala
@@ -7,9 +7,13 @@ import org.nlogo.headless.HeadlessWorkspace
 object SampleHeadless {
 
   def main( args: Array[String] ) {
+    oneWorkspace()
+    twoWorkspaces()
+  }
 
+  def oneWorkspace() {
     val workspace = HeadlessWorkspace.newInstance
-    workspace.open( "/Applications/NetLogo 6.1.0/models/Sample Models/Earth Science/Fire.nlogo" )
+    workspace.open( "/Applications/NetLogo 6.1.1/models/Sample Models/Earth Science/Fire.nlogo" )
 
     workspace.command( "set density 62" )
     workspace.command( "random-seed 0" )
@@ -22,6 +26,27 @@ object SampleHeadless {
     }
 
     println( workspace.report( "burned-trees" ) )
+  }
 
+  def twoWorkspaces() {
+    val workspace1 = HeadlessWorkspace.newInstance
+    workspace1.open( "/Applications/NetLogo 6.1.1/models/Sample Models/Earth Science/Fire.nlogo" )
+    val workspace2 = HeadlessWorkspace.newInstance
+    workspace2.open( "/Applications/NetLogo 6.1.1/models/Sample Models/Biology/Slime.nlogo" )
+
+    workspace1.command( "set density 62" )
+    workspace2.command( "set population 1000" )
+    workspace1.command( "random-seed 0" )
+    workspace2.command( "random-seed 0" )
+    workspace1.command( "setup" )
+    workspace2.command( "setup" )
+
+    (1 to 500).foreach( _ => {
+      workspace1.command( "go" )
+      workspace2.command( "go" )
+    })
+
+    println( workspace1.report( "burned-trees" ) )
+    println( workspace2.report( "mean [xcor] of turtles" ) )
   }
 }
